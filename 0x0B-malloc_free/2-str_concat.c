af7360fe10ebbaf30c24abeb42bc3598c5410b68
reyes <kingsudechukwu@gmail.com>
2022-06-28 01:41:44 -0700

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,7 +24,6 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-
 	i = 0;
 	while (*s1++)
 		i++;
@@ -55,4 +54,3 @@ char *str_concat(char *s1, char *s2)
 	str_copy[j] = '\0';
 	return (str_copy);
 }
-
