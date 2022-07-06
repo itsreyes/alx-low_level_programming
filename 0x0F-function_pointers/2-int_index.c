0918e6eeb54033bcad925062bf22e00406cafe9a
reyes <kingsudechukwu@gmail.com>
2022-07-06 08:23:40 -0700

    commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,15 +14,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && cmp)
+	if (size < 1 || array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]) != 0)
-			{
-				return (i);
-			}
-		}
+		if (cmp(array[i]))
+			return (i);
 	}
+
 	return (-1);
 }
