ff3602bc41de9aa01edf94c565c5d49f9cbac41b
reyes <kingsudechukwu@gmail.com>
2022-07-07 09:10:30 -0700

    commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,7 +14,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *clone;
 
 	va_start(arguments, n);
-	
 	for (s = 0; s < n; s++)
 	{
 		clone = va_arg(arguments, char*);
